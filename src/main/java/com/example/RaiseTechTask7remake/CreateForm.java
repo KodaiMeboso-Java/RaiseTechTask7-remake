5e0f37e3d1d16c603482884864d448204e2456ef
KodaiMeboso-Java <muxiguangda@gmail.com>
2023-03-12 21:35:21 +0900

    delete

---
+++ src/main/java/com/example/RaiseTechTask7remake/CreateForm.java
@@ -0,0 +1,13 @@
+package com.example.RaiseTechTask7remake;
+
+public class CreateForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
